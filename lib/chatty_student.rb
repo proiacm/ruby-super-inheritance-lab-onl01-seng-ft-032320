7b89297e5de58559097b0e1657c3a309acc569ec
CiaraMaria Proia <proiacm@gmail.com>
2020-04-08 06:27:16 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,13 @@
 require_relative "/.student.rb" 
 class Chatty_Student < Student 
   
+  def hello 
+    puts "Hey there! I'm so excited to learn stuff."
+  end 
   
+  def raise_hand 
+    puts "Pick me!"
+  end
   
   
 end
